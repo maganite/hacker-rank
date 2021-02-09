f3e921c90f8b79a3ea5af1ecfe0096b435564c72
coderharshmalik <harshmalik20015@gmail.com>
2021-02-10 00:06:29 +0530

    hacker-rank

---
+++ hackerrank2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    long b;
+    char ch;
+    float d;
+    double e;
+    cout.precision(10);
+    cin>>a>>b>>ch>>d>>e;
+    cout<<a<<"\n"<<b<<"\n"<<ch<<"\n"<<d<<"\n"<<e;
+    return 0;
+
+}
+
